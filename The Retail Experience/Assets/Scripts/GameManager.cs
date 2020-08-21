fcb19b5e8aa7bca6ad948248afeba25c08434015
LucasNeophytou <lucasneophytou@gmail.com>
2020-08-21 16:10:15 +1000

    Added states to GameManager script

--- The Retail Experience/Assets/Scripts/GameManager.cs
+++ The Retail Experience/Assets/Scripts/GameManager.cs
@@ -2,17 +2,69 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+
+public enum GameState {
+
+    menu,
+    inGame,
+    gameOver,
+    nextLevel
+}
+
 public class GameManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
+    
+    public GameState currentGameState = GameState.menu;
+    public static GameManager instance;
+    public Canvas menuCanvas;
+    public Canvas inGameCanvas;
+    public Canvas gameOverCanvas;
+    public Canvas nextLevelCanvas;
+
+
+    void Awake () {
+
+        instance = this;
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+    public void StartGame () {
+
+        SetGameState(GameState.inGame);
     }
+
+    public void SetGameState (GameState newGameState) {
+
+        if (newGameState == GameState.menu) {
+
+            menuCanvas.enabled = true;
+            inGameCanvas.enabled = false;
+            gameOverCanvas.enabled = false;
+            nextLevelCanvas.enabled = false;
+        }
+        else if (newGameState == GameState.inGame) {
+
+            menuCanvas.enabled = false;
+            inGameCanvas.enabled = true;
+            gameOverCanvas.enabled = false;
+            nextLevelCanvas.enabled = false;
+        }
+        else if (newGameState == GameState.gameOver) {
+
+            menuCanvas.enabled = false;
+            inGameCanvas.enabled = false;
+            gameOverCanvas.enabled = true;
+            nextLevelCanvas.enabled = false;
+        }
+        else if (newGameState == GameState.nextLevel) {
+
+            menuCanvas.enabled = false;
+            inGameCanvas.enabled = false;
+            gameOverCanvas.enabled = false;
+            nextLevelCanvas.enabled = true;
+        }
+
+        currentGameState = newGameState;
+    }
+
+
 }
